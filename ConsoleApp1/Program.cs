3f277b4707987bc38dc69fc3a28568344e04c91f
yiru <eva890706@gmail.com>
2022-11-07 17:53:53 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> input=Enumerable.Range(1,30).ToList();
+			Func<int, bool> func = n => n % 2 == 0;
+
+			List<int> result=GetEvenItems(input,func);
+			foreach (var item in result)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		static public List<int> GetEvenItems(List<int> input, Func<int, bool> func)
+		{
+			var result = new List<int>();
+			foreach (var item in input)
+			{
+				if (func(item)) result.Add(item);
+			}
+			return result;
+		}
+	}
+}
